336d20d3d84da24d76d9c26c0cc395935113c027
lhoplama1 <43850571+lhoplama1@users.noreply.github.com>
2020-06-10 18:46:19 -0500

    Add files via upload

---
+++ simpleWebServer.py
@@ -0,0 +1,50 @@
+import socket
+
+HOST = "127.0.0.1"
+PORT = 9000
+
+# socket.socket automatically creates a new socket
+with socket.socket() as server_sock:
+    # kernel reuses sockets that are TIME_WAIT
+    server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    # Binding:
+    server_sock.bind((HOST, PORT))
+
+    # 0 = pending connection the socket may have had before
+    # new connections are refused.  process one connection at a time
+    server_sock.listen(0)
+    print(f"Listening on {HOST}:{PORT}...")
+
+#socket connection with the client. block the process until a client connects to our server.
+
+    client_sock, client_addr = server_sock.accept()
+    print(f"New connection from {client_addr}.")
+
+
+# Allowing for there to be a refresh to the page instead of just exiting
+# Sending an example response
+RESPONSE = b"""\
+HTTP/1.1 200 OK
+content-type: text/html
+content-length: 15
+
+<h1>Hello!</h1>""".replace(b"\n", b"\r\n")
+
+with socket.socket() as server_sock:
+    server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_sock.bind((HOST, PORT))
+    server_sock.listen(0)
+    print(f"Listening on {HOST}:{PORT}...")
+
+    while True:
+        client_sock, client_addr = server_sock.accept()
+        print(f"new connection from {client_addr}.")
+        with client_sock:
+            client_sock.sendall(RESPONSE)
+
+#CAN SERVE BASIC HTML NOW YAAYAYAYAYAAYAYAYAYAYAYAAYAY
+
+
+
+
